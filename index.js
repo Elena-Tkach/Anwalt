a0b0bf9d3adee4e11cebb7402a547d63ada35cf8
‘Elena <tcachelena@gmail.com>
2024-11-11 09:55:28 +0500

    feat: Connect js-file

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 import { swiper } from "/scripts/swiper.js";
 import { handleClick } from "/scripts/router.js";
 import { years, slowScroll } from "/scripts/utils.js";
-
+import { sendMail } from "./scripts/sendMail";
 
 swiper();
 handleClick();
 years();
 slowScroll();
+sendMail();
